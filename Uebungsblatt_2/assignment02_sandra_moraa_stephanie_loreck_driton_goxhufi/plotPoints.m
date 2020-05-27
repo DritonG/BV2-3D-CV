47f8964178fcf667fe96db91643f4c2b14a091c2
DritonG <driton-goxhufi@web.de>
2020-05-27 19:30:56 +0200

    update ass02 ex c)

---
+++ Uebungsblatt_2/assignment02_sandra_moraa_stephanie_loreck_driton_goxhufi/plotPoints.m
@@ -0,0 +1,26 @@
+function plotPoints(points, edges, w, h)
+% draw figure by projected points
+% points - project points with depth information
+% edges - edge list of connected points, e.g. [1 2; 2 3; ...]
+% w, h - width and height of projection plane
+    
+% Place your code here...
+figure('Position', [0 0 w h]);
+title('plot-for-ass02');
+axis([0 w 0 h]);
+box on;
+hold on;
+
+for a=1:length(points)
+    if points(3,a) > 0
+        plot(points(1,a), points(2,a), '*');
+    end
+end
+for a=1:length(edges)
+    if points(3, edges(a,1)) > 0 || points(3, edges(a,2)) > 0
+        line([points(1, edges(a,1)) points(1, edges(a,2))],[points(2, edges(a,1)) points(2, edges(a,2))], 'Color', 'red');
+    end
+end
+hold on;
+end
+
